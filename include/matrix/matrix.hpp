b4d09833abb6c72b5677ef59ff980c39630b481b
Prosper A <prosperaigbe345@gmail.com>
2026-01-19 23:01:46 +0100

    Add mixed-type join operation and operator| for Vector
    - Implemented join() for concatenating vectors of different numeric types
      using Numeric<T, V> promotion
    - Added corresponding operator| overload
    - Preserves type safety and efficient memory allocation

--- include/matrix/matrix.hpp
+++ include/matrix/matrix.hpp
@@ -1408,10 +1408,9 @@ public:
   Matrix<V> reshape(std::size_t rows, std::size_t columns) const;
 
   Vector<V> join(const Vector<V> &other) const {
-
-    Vector<V> result(data.size() + other.data.size());
     const std::size_t this_size = data.size();
     const std::size_t other_size = other.data.size();
+    Vector<V> result(this_size + other_size);
 
     std::copy(data.data(), data.data() + this_size, result.data.data());
     std::copy(other.data.data(), other.data.data() + other_size,
@@ -1419,7 +1418,29 @@ public:
 
     return result;
   }
+
+  template <NumericType T>
+  Vector<Numeric<T, V>> join(const Vector<T> &other) const {
+    const std::size_t this_size = data.size();
+    const std::size_t other_size = other.data.size();
+    Vector<Numeric<T, V>> result(this_size + other_size);
+
+    for (std::size_t i = 0; i < this_size; ++i) {
+      result[i] = data[i];
+    }
+
+    for (std::size_t i = 0; i < other_size; ++i) {
+      result[this_size + i] = other[i];
+    }
+
+    return result;
+  }
   Vector<V> operator|(const Vector<V> &other) const { return join(other); }
+
+  template <NumericType T>
+  Vector<Numeric<T, V>> operator|(const Vector<T> &other) const {
+    return join(other);
+  }
 
   // Iterators
   using iterator = typename std::vector<V>::iterator;
