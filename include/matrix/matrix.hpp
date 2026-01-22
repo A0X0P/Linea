b4510bc765b30b052f46ea7208a2a964df3eb188
Prosper A <prosperaigbe345@gmail.com>
2026-01-22 11:22:04 +0100

    Refactor numeric type concept into distinct Real and Integral concepts
    - Split `NumericType` into `RealType` (double, long double) and `IntegralType` (integral types ≥ int, excluding bool/char variants)
    - Redefine `NumericType` as a combination of `IntegralType` and `RealType`
    - Improves type safety and clarity for template constraints

--- include/matrix/matrix.hpp
+++ include/matrix/matrix.hpp
@@ -20,12 +20,18 @@
 namespace Linea {
 
 template <typename T>
-concept NumericType =
-    (std::is_integral_v<T> || std::is_floating_point_v<T>) &&
-    !std::is_same_v<T, bool> && !std::is_same_v<T, char> &&
-    !std::is_same_v<T, signed char> && !std::is_same_v<T, unsigned char> &&
-    !std::is_same_v<T, wchar_t> && !std::is_same_v<T, char8_t> &&
-    !std::is_same_v<T, char16_t> && !std::is_same_v<T, char32_t>;
+concept RealType = std::is_same_v<T, double> || std::is_same_v<T, long double>;
+
+template <typename T>
+concept IntegralType =
+    std::is_integral_v<T> && !std::is_same_v<T, bool> &&
+    !std::is_same_v<T, char> && !std::is_same_v<T, signed char> &&
+    !std::is_same_v<T, unsigned char> && !std::is_same_v<T, wchar_t> &&
+    !std::is_same_v<T, char8_t> && !std::is_same_v<T, char16_t> &&
+    !std::is_same_v<T, char32_t> && (sizeof(T) >= sizeof(int));
+
+template <typename T>
+concept NumericType = IntegralType<T> || RealType<T>;
 
 template <NumericType T, NumericType S>
 using Numeric = std::common_type_t<T, S>;
