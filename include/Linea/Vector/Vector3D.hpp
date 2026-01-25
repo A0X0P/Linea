19ab02051d70ed205c4d1618a33edec7f0b80046
Prosper A <prosperaigbe345@gmail.com>
2026-01-25 15:20:19 +0100

    vector(Vector3D.hpp): implement fixed-size 3D vector class
    - Provide constructors from std::array, list, and Linea::Vector
    - Add element access via operator[]
    - Implement cross product, scalar triple, and vector triple product
    - Provide raw array access for internal data

--- include/Linea/Vector/Vector3D.hpp
+++ include/Linea/Vector/Vector3D.hpp
@@ -1,3 +1,66 @@
 // created by : A.N. Prosper
 // date : january 25th 2026
 // time : 00:01
+
+#ifndef LINEA_VECTOR3D_H
+#define LINEA_VECTOR3D_H
+
+#include "../Core/Concepts.hpp"
+#include <array>
+
+namespace Linea {
+
+template <NumericType V> class Vector;
+
+template <NumericType T> class Vector3D {
+
+private:
+  std::array<T, 3> data{};
+
+public:
+  // Constructors
+
+  // std::array initialization
+  constexpr explicit Vector3D(const std::array<T, 3> &arr) : data(arr) {}
+
+  // list initialization
+  constexpr Vector3D(T x, T y, T z) : data{x, y, z} {}
+
+  // Linea::Vector initialization
+  explicit Vector3D(const Vector<T> &v);
+
+  // Element Access
+  constexpr T &operator[](std::size_t i) noexcept { return data[i]; }
+  constexpr const T &operator[](std::size_t i) const noexcept {
+    return data[i];
+  }
+
+  // Cross Product
+  constexpr Vector3D<T> cross(const Vector3D<T> &other) const {
+    return Vector3D<T>{data[1] * other[2] - data[2] * other[1],
+                       data[2] * other[0] - data[0] * other[2],
+                       data[0] * other[1] - data[1] * other[0]};
+  }
+
+  // Scalar Triple Product
+  // (a · (b × c))
+  constexpr T scalar_triple_product(const Vector3D &b,
+                                    const Vector3D &c) const {
+    const Vector3D result = b.cross(c);
+    return data[0] * result[0] + data[1] * result[1] + data[2] * result[2];
+  }
+
+  // Vector Triple Product
+  //(a × (b × c))
+  constexpr Vector3D vector_triple_product(const Vector3D &b,
+                                           const Vector3D &c) const {
+    return this->cross(b.cross(c));
+  }
+
+  // Raw Access
+  constexpr const std::array<T, 3> &getdata() const noexcept { return data; }
+};
+
+} // namespace Linea
+
+#endif // LINEA_VECTOR3D_H
