85523a9fd4a8145013e3a45c0fd8f0db3a7bc523
Prosper A <prosperaigbe345@gmail.com>
2026-01-29 19:51:41 +0100

    chore: add centralized Linea.hpp header for unified engine access
    Introduced Linea.hpp to include all core, vector, matrix, decomposition,
    and IO headers in a single file. This simplifies user access and
    streamlines inclusion across projects, providing a central entry point
    for the Linea matrix and vector engine.

--- include/Linea.hpp
+++ include/Linea.hpp
@@ -1,3 +1,35 @@
 // created by : A.N. Prosper
 // date : January 26th 2026
 // time : 10:00
+
+#ifndef LINEA_H
+#define LINEA_H
+
+// Core
+#include "Linea/Core/Concepts.hpp"
+#include "Linea/Core/Types.hpp"
+#include "Linea/Core/Utilities.hpp"
+
+// Vector
+#include "Linea/Vector/Vector.hpp"
+#include "Linea/Vector/Vector3D.hpp"
+#include "Linea/Vector/VectorMath.hpp"
+#include "Linea/Vector/VectorOperations.hpp"
+
+// Matrix
+#include "Linea/Matrix/Matrix.hpp"
+#include "Linea/Matrix/MatrixMath.hpp"
+#include "Linea/Matrix/MatrixOperations.hpp"
+#include "Linea/Matrix/MatrixUtilities.hpp"
+
+// Decomposition
+
+#include "Linea/Decompositions/Cholesky.hpp"
+#include "Linea/Decompositions/HouseholderQR.hpp"
+#include "Linea/Decompositions/LUFactor.hpp"
+#include "Linea/Decompositions/SVD.hpp"
+
+// IO
+#include "Linea/IO/StreamOperations.hpp"
+
+#endif // LINEA_H
